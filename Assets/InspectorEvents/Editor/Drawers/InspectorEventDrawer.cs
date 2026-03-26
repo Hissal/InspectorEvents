801757472bfb1b9c59aaaf81c746d93779768a16
Lassi Harjaluoma <hissalofficial@gmail.com>
2026-03-26 13:38:21 +0200

    feat: enhance InspectorEventDrawer with inline invoke button

--- Assets/InspectorEvents/Editor/Drawers/InspectorEventDrawer.cs
+++ Assets/InspectorEvents/Editor/Drawers/InspectorEventDrawer.cs
@@ -6,15 +6,32 @@ using UnityEngine;
 
 namespace InspectorEvents.Editor.Drawers {
     public sealed class InspectorEventDrawer : OdinValueDrawer<InspectorEvent> {
+        const float c_headerButtonWidth = 54f;
+        const float c_headerButtonGap = 6f;
+
         protected override void DrawPropertyLayout(GUIContent label) {
-            DrawInlineFoldout(Property);
+            DrawInlineFoldout(Property, ValueEntry.SmartValue);
         }
 
         // Draw a FoldoutGroup-like boxed block with a left foldout toggle and nicified property title.
-        static void DrawInlineFoldout(InspectorProperty property) {
+        static void DrawInlineFoldout(InspectorProperty property, InspectorEvent inspectorEvent) {
             SirenixEditorGUI.BeginBox();
             SirenixEditorGUI.BeginBoxHeader();
-            property.State.Expanded = EditorGUILayout.Foldout(property.State.Expanded, property.NiceName, true);
+
+            var headerRect = EditorGUILayout.GetControlRect(false, EditorGUIUtility.singleLineHeight);
+            var foldoutWidth = Mathf.Max(0f, headerRect.width - c_headerButtonWidth - c_headerButtonGap);
+            var foldoutRect = new Rect(headerRect.x, headerRect.y, foldoutWidth, headerRect.height);
+
+            var buttonHeight = Mathf.Min(18f, Mathf.Max(0f, headerRect.height - 2f));
+            var buttonY = headerRect.y + (headerRect.height - buttonHeight) * 0.5f;
+            var buttonRect = new Rect(headerRect.xMax - c_headerButtonWidth, buttonY, c_headerButtonWidth, buttonHeight);
+
+            property.State.Expanded = EditorGUI.Foldout(foldoutRect, property.State.Expanded, property.NiceName, true);
+
+            if (GUI.Button(buttonRect, "Invoke", EditorStyles.miniButton)) {
+                inspectorEvent?.Invoke();
+            }
+
             SirenixEditorGUI.EndBoxHeader();
 
             if (property.State.Expanded) {
