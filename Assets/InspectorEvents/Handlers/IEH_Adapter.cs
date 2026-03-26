e5baa0f78b1e1c599d34676bbe101c1c6edf66e3
Lassi Harjaluoma <hissalofficial@gmail.com>
2026-03-26 15:33:19 +0200

    feat: add non-generic adapter for inspector event handling

---
+++ Assets/InspectorEvents/Handlers/IEH_Adapter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using InspectorEvents.Core;
+using UnityEngine;
+
+namespace InspectorEvents.Listeners;
+
+[Serializable]
+public sealed class IEH_ToNonGenericAdapter<TEvent> : IInspectorEventHandler<TEvent> {
+    [SerializeReference] IInspectorEventHandler nonGenericHandler;
+    public void Handle(in TEvent e) => nonGenericHandler.Handle();
+}
