86d1735b270bcc32a7b944702fea45bbde1acee4
Lassi Harjaluoma <hissalofficial@gmail.com>
2026-04-03 18:30:22 +0300

    feat: add IEF_Chance filter for probabilistic event handling

---
+++ Assets/InspectorEvents/Filters/IEF_Chance.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using InspectorEvents.Core;
+using UnityEngine;
+
+namespace InspectorEvents.Filters;
+
+[Serializable]
+public sealed class IEF_Chance : IInspectorEventFilter {
+    [SerializeField, Range(0f, 1f)] float chance = 0.5f;
+
+    public bool Evaluate() {
+        return ChanceFilterUtility.Evaluate(chance);
+    }
+}
+
+[Serializable]
+public sealed class IEF_Chance<TEvent> : IInspectorEventFilter<TEvent> {
+    [SerializeField, Range(0f, 1f)] float chance = 0.5f;
+
+    public bool Evaluate(in TEvent evt) {
+        return ChanceFilterUtility.Evaluate(chance);
+    }
+}
+
+internal static class ChanceFilterUtility {
+    public static bool Evaluate(float chance) {
+        var normalizedChance = Mathf.Clamp01(chance);
+        return normalizedChance switch {
+            <= 0f => false,
+            >= 1f => true,
+            _ => UnityEngine.Random.value < normalizedChance
+        };
+    }
+}
