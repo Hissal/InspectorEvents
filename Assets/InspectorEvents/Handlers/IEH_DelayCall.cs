3a00e30000222686b2e0f3941ad32ef5c0a0d246
Lassi Harjaluoma <hissalofficial@gmail.com>
2026-04-03 18:25:05 +0300

    feat: add delay and filter call handlers for event processing

---
+++ Assets/InspectorEvents/Handlers/IEH_DelayCall.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using InspectorEvents.Core;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace InspectorEvents.Handlers;
+
+[Serializable]
+public sealed class IEH_DelayCall : IInspectorEventHandler {
+    [SerializeField] bool randomize;
+    [SerializeField, HideIf(nameof(randomize))] float delaySeconds;
+    [SerializeField, ShowIf(nameof(randomize))] Vector2 delaySecondsRange;
+    [PropertySpace]
+    [SerializeReference] 
+    IInspectorEventHandler[] handlers = Array.Empty<IInspectorEventHandler>();
+    
+    public void Handle() {
+        var delay = randomize ? UnityEngine.Random.Range(delaySecondsRange.x, delaySecondsRange.y) : delaySeconds;
+        HandleDelayed(delay);
+    }
+
+    async void HandleDelayed(float delay) {
+        try {
+            await Awaitable.WaitForSecondsAsync(delay);
+            foreach (var handler in handlers) {
+                handler.Handle();
+            }
+        }
+        catch (Exception e) {
+            Debug.LogException(e);
+        }
+    }
+}
+
+[Serializable]
+public sealed class IEH_DelayCall<TEvent> : IInspectorEventHandler<TEvent> {
+    [SerializeField] bool randomize;
+    [SerializeField, HideIf(nameof(randomize))] float delaySeconds;
+    [SerializeField, ShowIf(nameof(randomize))] Vector2 delaySecondsRange;
+    [PropertySpace]
+    [SerializeReference] 
+    IInspectorEventHandler<TEvent>[] handlers = Array.Empty<IInspectorEventHandler<TEvent>>();
+    
+    public void Handle(in TEvent evt) {
+        var delay = randomize ? UnityEngine.Random.Range(delaySecondsRange.x, delaySecondsRange.y) : delaySeconds;
+        HandleDelayed(delay, evt);
+    }
+
+    async void HandleDelayed(float delay, TEvent evt) {
+        try {
+            await Awaitable.WaitForSecondsAsync(delay);
+            foreach (var handler in handlers) {
+                handler.Handle(evt);
+            }
+        }
+        catch (Exception e) {
+            Debug.LogException(e);
+        }
+    }
+}
---
+++ Assets/InspectorEvents/Handlers/IEH_FilterCall.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using InspectorEvents.Core;
+using UnityEngine;
+
+namespace InspectorEvents.Handlers;
+
+[Serializable]
+public sealed class IEH_FilterCall : IInspectorEventHandler {
+    [SerializeReference] 
+    IInspectorEventFilter[] filters = Array.Empty<IInspectorEventFilter>();
+    [SerializeReference] 
+    IInspectorEventHandler[] handlers = Array.Empty<IInspectorEventHandler>();
+    
+    public void Handle() {
+        // ReSharper disable once LoopCanBeConvertedToQuery
+        foreach (var filter in filters) {
+            if (!filter.Evaluate()) {
+                return;
+            }
+        }
+
+        foreach (var handler in handlers) {
+            handler.Handle();
+        }
+    }
+}
+
+[Serializable]
+public sealed class IEH_FilterCall<TEvent> : IInspectorEventHandler<TEvent> {
+    [SerializeReference] 
+    IInspectorEventFilter<TEvent>[] filters = Array.Empty<IInspectorEventFilter<TEvent>>();
+    [SerializeReference] 
+    IInspectorEventHandler<TEvent>[] handlers = Array.Empty<IInspectorEventHandler<TEvent>>();
+    
+    public void Handle(in TEvent evt) {
+        // ReSharper disable once LoopCanBeConvertedToQuery
+        foreach (var filter in filters) {
+            if (!filter.Evaluate(evt)) {
+                return;
+            }
+        }
+
+        foreach (var handler in handlers) {
+            handler.Handle(evt);
+        }
+    }
+}
