b7bc82fa518fefbcae2e904ff969a3493566a4d2
Lassi Harjaluoma <hissalofficial@gmail.com>
2026-04-03 18:39:49 +0300

    feat: rename chance variable to passChance for clarity

--- Assets/InspectorEvents/Filters/IEF_Chance.cs
+++ Assets/InspectorEvents/Filters/IEF_Chance.cs
@@ -1,34 +1,35 @@
 ﻿using System;
 using InspectorEvents.Core;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace InspectorEvents.Filters;
 
 [Serializable]
 public sealed class IEF_Chance : IInspectorEventFilter {
-    [SerializeField, Range(0f, 1f)] float chance = 0.5f;
+    [FormerlySerializedAs("chance")] [SerializeField, Range(0f, 1f)] float passChance = 0.5f;
 
     public bool Evaluate() {
-        return ChanceFilterUtility.Evaluate(chance);
+        return ChanceFilterUtility.Evaluate(passChance);
     }
 }
 
 [Serializable]
 public sealed class IEF_Chance<TEvent> : IInspectorEventFilter<TEvent> {
-    [SerializeField, Range(0f, 1f)] float chance = 0.5f;
+    [FormerlySerializedAs("chance")] [SerializeField, Range(0f, 1f)] float passChance = 0.5f;
 
     public bool Evaluate(in TEvent evt) {
-        return ChanceFilterUtility.Evaluate(chance);
+        return ChanceFilterUtility.Evaluate(passChance);
     }
 }
 
 internal static class ChanceFilterUtility {
-    public static bool Evaluate(float chance) {
-        var normalizedChance = Mathf.Clamp01(chance);
-        return normalizedChance switch {
+    public static bool Evaluate(float passChance) {
+        var normalizedPassChance = Mathf.Clamp01(passChance);
+        return normalizedPassChance switch {
             <= 0f => false,
             >= 1f => true,
-            _ => UnityEngine.Random.value < normalizedChance
+            _ => UnityEngine.Random.value < normalizedPassChance
         };
     }
 }
